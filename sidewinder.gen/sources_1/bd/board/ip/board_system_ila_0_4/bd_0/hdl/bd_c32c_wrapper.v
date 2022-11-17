27a83880f50709b02bf8a6742bec3105939cddd3
DouglasWWolf <douglaswolf@yahoo.com>
2022-11-17 03:52:18 -0800

    No commit message

---
+++ sidewinder.gen/sources_1/bd/board/ip/board_system_ila_0_4/bd_0/hdl/bd_c32c_wrapper.v
@@ -0,0 +1,33 @@
+//Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Command: generate_target bd_c32c_wrapper.bd
+//Design : bd_c32c_wrapper
+//Purpose: IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module bd_c32c_wrapper
+   (SLOT_0_AXIS_tdata,
+    SLOT_0_AXIS_tlast,
+    SLOT_0_AXIS_tvalid,
+    clk,
+    resetn);
+  input [255:0]SLOT_0_AXIS_tdata;
+  input SLOT_0_AXIS_tlast;
+  input SLOT_0_AXIS_tvalid;
+  input clk;
+  input resetn;
+
+  wire [255:0]SLOT_0_AXIS_tdata;
+  wire SLOT_0_AXIS_tlast;
+  wire SLOT_0_AXIS_tvalid;
+  wire clk;
+  wire resetn;
+
+  bd_c32c bd_c32c_i
+       (.SLOT_0_AXIS_tdata(SLOT_0_AXIS_tdata),
+        .SLOT_0_AXIS_tlast(SLOT_0_AXIS_tlast),
+        .SLOT_0_AXIS_tvalid(SLOT_0_AXIS_tvalid),
+        .clk(clk),
+        .resetn(resetn));
+endmodule
